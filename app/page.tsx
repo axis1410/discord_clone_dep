a757c183e36ffe7d55478436e7c6850ee2e2faa6
Aditya <adityasingh14102001@gmail.com>
2024-10-06 21:46:40 +0530

    Initialize Clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 const HomePage = () => {
-	return <div>HomePage</div>;
+	return (
+		<div>
+			HomePage
+			<UserButton afterSignOutUrl="/" />
+		</div>
+	);
 };
 
 export default HomePage;
